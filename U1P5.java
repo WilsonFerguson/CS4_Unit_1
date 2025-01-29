1bf52dade7329eae6871d0c5499bac7b6693a044
BuildTools <unconfigured@null.spigotmc.org>
2025-01-28 17:44:20 -0800

    added U1P5 and U1P6

---
+++ U1P5.java
@@ -0,0 +1,6 @@
+import java.util.*;
+
+class U1P5 {
+    public static void main(String[] args) {
+    }
+}
---
+++ U1P6.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+class U1P6 {
+    public static void main(String[] args) {
+    }
+}
