4edd0f9f71a3ffc614795203d8de29a0b33995ea
BuildTools <unconfigured@null.spigotmc.org>
2025-01-28 17:35:42 -0800

    U1P4 mostly done

--- U1P4.java
+++ U1P4.java
@@ -1,6 +1,34 @@
-import java.util.Stack;
+import java.util.*;
 
 class U1P4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter a string: ");
+            String input = scanner.next();
+
+            // Or just ctrl + c to kill it
+            if (input.equals("exit")) {
+                break;
+            }
+
+            SimpleLinkedStack<Character> stack = new SimpleLinkedStack<>();
+            for (char c : input.toCharArray()) {
+                if (c != '-') {
+                    stack.push(c);
+                } else {
+                    if (stack.isEmpty()) {
+                        System.out.print("!");
+                        break;
+                    } else {
+                        System.out.print(stack.pop());
+                    }
+                }
+            }
+            System.out.println("");
+
+        }
+        scanner.close();
     }
 }
