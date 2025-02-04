52220eb26c9506363981bad3edb9e00e497a3581
BuildTools <unconfigured@null.spigotmc.org>
2025-02-03 18:43:35 -0800

    Cleaned up U1P8

--- U1P8.java
+++ U1P8.java
@@ -3,16 +3,16 @@ class U1P8 {
     public static void main(String[] args) {
         int numGenerated = 0;
         SimpleLinkedQueue<Integer> queue = new SimpleLinkedQueue<>();
-        SimpleLinkedStack<Integer> stack = new SimpleLinkedStack<>();
         queue.push(1);
+        int last = 1;
         while (numGenerated++ < 100) {
             int item = queue.pull();
-            // System.out.print(item);
-            stack.push(item);
+            last = item;
+
             queue.push(5 * item);
             queue.push(5 * item + 1);
         }
 
-        System.out.println(stack.peek());
+        System.out.println(last);
     }
 }
